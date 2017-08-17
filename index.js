2778f31bd29682e5d33155fe23efb9149dcf251d
minwonki <wk.min@braincommerce.com>
2017-08-17 16:59:09 +0900

    3rd commit

---
+++ index.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+var cron = require('node-cron');
+var request = require('request');
+
+var sampleStr = 'nothing';
+var startValue = 4920250;
+var range = 340000;
+var flagStop = false;
+
+cron.schedule('*/10 * * * *', function() {
+  var date = new Date();
+  var current_hour = date.getHours(); 
+  if (current_hour == 1) {
+      request('https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=btc_krw',
+      function(error, response, body) {
+          var jsonObj = JSON.parse(body);
+          startValue = jsonObj['last'];
+          range = jsonObj['high'] - jsonObj['low'];
+          console.log('start:',startValue);
+          console.log('range:',range);
+          flagStop = false;
+      });
+  }
+
+  if (flagStop == false) {
+    request('https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=btc_krw',
+        function(error, response, body) {
+            var jsonObj = JSON.parse(body);
+            console.log('last:',jsonObj['last']);
+            var buy = startValue + range*0.5;
+            var last = jsonObj['last']
+            if (buy < last) {
+               sampleStr = 'Buy Now!!-' + last; 
+               console.log('Buy Now!!-',last);
+               flagStop = true;
+            } else {
+               sampleStr = 'Not yet - ' + buy + ':' + last;
+            }
+    });
+  }
+  
+},
+function(){console.log('job stop')},
+true);
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World! </br>' + sampleStr);
+});
+ 
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+ 
+  console.log('앱은 http://%s:%s 에서 작동 중입니다.', host, port);
+});
