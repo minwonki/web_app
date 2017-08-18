498eb1f7e79b8bd0b2b0704101988916e865328c
minwonki <wk.min@braincommerce.com>
2017-08-18 11:41:06 +0900

    modify log

--- index.js
+++ index.js
@@ -4,42 +4,35 @@ var cron = require('node-cron');
 var request = require('request');
 
 var sampleStr = 'nothing';
-var startValue = 4920250;
-var range = 340000;
+var startValue = 4850250;
+var range = 200000;
 var flagStop = false;
 
-cron.schedule('*/10 * * * *', function() {
-  var date = new Date();
-  var current_hour = date.getHours(); 
-  if (current_hour == 1) {
-      request('https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=btc_krw',
-      function(error, response, body) {
-          var jsonObj = JSON.parse(body);
-          startValue = jsonObj['last'];
-          range = jsonObj['high'] - jsonObj['low'];
-          console.log('start:',startValue);
-          console.log('range:',range);
-          flagStop = false;
-      });
-  }
-
-  if (flagStop == false) {
+cron.schedule('*/2 * * * *', function() {
     request('https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=btc_krw',
-        function(error, response, body) {
-            var jsonObj = JSON.parse(body);
+    function(error, response, body) {
+        var jsonObj = JSON.parse(body);
+        var timestamp = new Date(jsonObj['timestamp']);
+        if (timestamp.getHours() == 1) {
+            console.log('time:',timestamp.getHours());
+            startValue = jsonObj['last'];
+            range = jsonObj['high'] - jsonObj['low'];
+            console.log('start:', startValue);
+            console.log('range:', range);
+            console.log('time:', timestamp.toString());
+            sampleStr = 'Setting StartValue:' + startValue + ', range:' + range + ', time:' + timestamp.toString();
+        } else {
             var buy = startValue + range*0.5;
             var last = jsonObj['last']
             if (buy < last) {
-               sampleStr = 'Buy Now!!-' + last; 
-               console.log('Buy Now!!-',last);
-               flagStop = true;
+               sampleStr = 'Buy Now!! - last:' + last + ',buy:' + buy + ' - ' +timestamp.toString(); 
+               console.log('Buy Now!! - last:' + last + ',buy:' + buy + ' - ' +timestamp.toString());
             } else {
-               sampleStr = 'Not yet - ' + buy + ':' + last;
-               console.log('Not yet - last:',jsonObj['last']+",buy:"+buy);
+               sampleStr = 'Not yet - last:' + last + ',buy:' + buy + ' - ' +timestamp.toString();
+               console.log('Not yet - last:' + last + ',buy:' + buy + ' - ' +timestamp.toString());
             }
+        }
     });
-  }
-  
 },
 function(){console.log('job stop')},
 true);
