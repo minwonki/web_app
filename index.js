a5b517177d550f422278d1e82e018f9c343365cb
minwonki <wk.min@braincommerce.com>
2017-08-29 14:14:32 +0900

    update fcm function

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ var express = require('express');
 var app = express();
 var cron = require('node-cron');
 var request = require('request');
+var FCM = require('fcm-push');
+var sendFCM_btc = 1;
+var sendFCM_eth = 1;
+
+var serverKey= '';
+var fcm = new FCM(serverKey);
 
 var btcStr = 'btc_nothing';
 var ethStr = 'eth_nothing';
@@ -9,6 +15,7 @@ var btc_startVal = 100;
 var btc_rangeVal = 10;
 var eth_startVal = 101;
 var eth_rangeVal = 11;
+
 
 function Req (type) {
   this.url = 'https://api.korbit.co.kr/v1/ticker/detailed?currency_pair=' + type;
@@ -18,6 +25,8 @@ function Req (type) {
       var jsonObj = JSON.parse(body);
       var timestamp = new Date(jsonObj['timestamp']);
       if (timestamp.getHours() == 1) {
+          sendFCM_btc = 1;
+          sendFCM_eth = 1;
           if (type == 'btc_krw') {
               btc_startVal = jsonObj['last'];
               btc_rangeVal = jsonObj['high'] - jsonObj['low'];
@@ -29,16 +38,43 @@ function Req (type) {
           var buy = 0;
           var last = jsonObj['last']
           sampleStr = timestamp.toString();
+          sampleTitle = "";
           if (type == 'btc_krw') {
               buy = Number(btc_startVal) + btc_rangeVal*0.5;
               sampleStr = sampleStr + '</br>start:' + btc_startVal + ', range:' + btc_rangeVal + ", last:" + last; 
+              sampleTitle = "[BTC]";
           } else {
               buy = Number(eth_startVal) + eth_rangeVal*0.5;
               sampleStr = sampleStr + '</br>start:' + eth_startVal + ', range:' + eth_rangeVal + ", last:" + last; 
+              sampleTitle = "[ETH]";
           }
 
           if (buy < last) {
              sampleStr = sampleStr + '</br>Buy Now!!! - buy:' + buy; 
+             var message = {
+               to: '',
+               collapse_key: 'your_collapse_key',
+               data: {
+                 your_custom_data_key: 'your_custom_data_value'
+               },
+               notification: {
+                 title: sampleTitle,
+                 body: sampleStr  
+               }
+             };
+             if (type == 'btc_krw') {
+               if (sendFCM_btc > 0) {
+                 sendFCM_btc = sendFCM_btc - 1;
+                 fcm.send(message, function(err, response) {
+                 });
+               }
+             } else {
+               if (sendFCM_eth > 0) {
+                 sendFCM_eth = sendFCM_eth - 1;
+                 fcm.send(message, function(err, response) {
+                 });
+               }
+             }
           } else {
              sampleStr = sampleStr + '</br>Not yet!!! - buy:' + buy;
           }
@@ -54,7 +90,7 @@ function Req (type) {
   });
 }
 
-cron.schedule('*/10 * * * *', function() {
+cron.schedule('*/2 * * * *', function() {
     Req('btc_krw');
     Req('eth_krw');
 },
